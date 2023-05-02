21a6dc8194452d0fdc236ec3e75c50a5a393cfdc
Yogev282 <yogev282000@gmail.com>
2023-05-02 21:38:44 +0300

    final - bash grade 100

--- Demo.cpp
+++ Demo.cpp
@@ -17,33 +17,20 @@ using namespace ariel;
 
 
 int main() {
-    // Fraction a(5,3), b(14,21);
-    // cout << "a: " << a << "b: " << b << endl;
-    // cout << "a+b" << a+b << endl; 
-    // cout << "a-b" << a-b << endl; 
-    // cout << "a/b" << a/b << endl; 
-    // cout << "a*b" << a*b << endl; 
-    // cout << "2.3*b" << 2.3*b << endl; 
-    // cout << "a+2.421" << a+2.421 << endl; 
-    // Fraction c = a+b-1;
-    // cout << c++ << endl;
-    // cout << --c << endl;
+    Fraction a(5,3), b(14,21);
+    cout << "a: " << a << "b: " << b << endl;
+    cout << "a+b" << a+b << endl; 
+    cout << "a-b" << a-b << endl; 
+    cout << "a/b" << a/b << endl; 
+    cout << "a*b" << a*b << endl; 
+    cout << "2.3*b" << 2.3*b << endl; 
+    cout << "a+2.421" << a+2.421 << endl; 
+    Fraction c = a+b-1;
+    cout << c++ << endl;
+    cout << --c << endl;
 
-    // cout << "c >=b ? : " << (c >= b) << endl;
-    // if (a > 1.1) cout << " a is bigger than 1.1" << endl;
-    // else cout << " a is smaller than 1.1" << endl;
-    int max_int = std::numeric_limits<int>::max();
-    int min_int = std::numeric_limits<int>::min();
-
-    Fraction b(min_int, min_int);
-    cout << "b: " << b << endl;
-    
-    Fraction a(max_int, max_int);
-    cout << "a: " << a << endl;
-
-    
-    
-    
-
+    cout << "c >=b ? : " << (c >= b) << endl;
+    if (a > 1.1) cout << " a is bigger than 1.1" << endl;
+    else cout << " a is smaller than 1.1" << endl;
 
 }
